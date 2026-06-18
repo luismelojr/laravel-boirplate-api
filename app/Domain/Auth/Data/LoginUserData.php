5111f3b90678e3164ff01b67b354ba7fa40ba4bc
Luis Henrique <junimhs1996@gmail.com>
2026-06-18 10:44:47 -0300

    feat: replace manual DTOs with spatie/laravel-data base class

--- app/Domain/Auth/Data/LoginUserData.php
+++ app/Domain/Auth/Data/LoginUserData.php
@@ -4,18 +4,12 @@ declare(strict_types=1);
 
 namespace App\Domain\Auth\Data;
 
-readonly class LoginUserData
+use Spatie\LaravelData\Data;
+
+class LoginUserData extends Data
 {
     public function __construct(
         public readonly string $email,
         public readonly string $password,
     ) {}
-
-    public static function from(array $data): self
-    {
-        return new self(
-            email: $data['email'],
-            password: $data['password'],
-        );
-    }
 }
